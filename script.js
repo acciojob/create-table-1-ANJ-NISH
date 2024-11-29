88c0f9c61c9c6cca2c3278ca703ff4a513244fbb
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-29 18:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+
+	let tb=document.getElementById("sampleTable");
+	
+    let tr=document.createElement('tr');
+	let td1=document.createElement('td');
+	let td2=document.createElement('td');
+
+	let text1=document.createTextNode('New Cell1');
+	let text2=document.createTextNode('New Cell2');
+
+	td1.appendChild(text1);
+	td2.appendChild(text2);
+
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+
+	
+  tb.insertAdjacentElement('afterbegin',tr);
 }
